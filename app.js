4efc543e0234f0760af919491223e1ce0378b286
Tsousa20 <alexandre.gueidao.14@gmail.com>
2024-05-23 14:10:04 +0100

    Update 23/05

--- app.js
+++ app.js
@@ -51,12 +51,24 @@ app.get('/', async (req, res) => {
         const results3 = await executeQuery(query3);
 
         // Quarta query
-        const query4 = 'SELECT p.*, c.company_name AS company_name FROM products p JOIN companies c ON p.company_id = c.id ORDER BY RAND() LIMIT 16;';
+        const query4 = 'SELECT p.*, c.company_name AS company_name FROM products p JOIN companies c ON p.company_id = c.id ORDER BY RAND() LIMIT 16';
         const products = await executeQuery(query4);
 
-        // Dividir os produtos em dois grupos de 4 produtos cada
-        const productsGroup1 = products.slice(0, 4);
-        const productsGroup2 = products.slice(4, 8);
+        // Quinta query
+        const query5 = 'SELECT p.* FROM products p JOIN sub_departments sd ON p.sub_department_id = sd.id JOIN departments d ON sd.department_id = d.id WHERE d.id = 2 ORDER BY RAND() LIMIT 5';
+        const results5 = await executeQuery(query5);
+
+        // Sexta query
+        const query6 = 'SELECT p.* FROM products p JOIN sub_departments sd ON p.sub_department_id = sd.id JOIN departments d ON sd.department_id = d.id WHERE d.id = 3 ORDER BY RAND() LIMIT 5';
+        const results6 = await executeQuery(query6);
+
+        // Setima query
+        const query7 = 'SELECT p.* FROM products p JOIN sub_departments sd ON p.sub_department_id = sd.id JOIN departments d ON sd.department_id = d.id WHERE d.id = 6 ORDER BY RAND() LIMIT 5';
+        const results7 = await executeQuery(query7);
+
+        // Dividir os produtos em dois grupos de 4 produtos cada (Quarta query)
+        const productsGroup1 = products.slice(0, 6);
+        const productsGroup2 = products.slice(6, 12);
         const productsGroup3 = products.slice(8, 16);
 
         // Formatar os preços com duas casas decimais
@@ -68,7 +80,7 @@ app.get('/', async (req, res) => {
         });
 
         // Renderizar a página EJS com os resultados
-        res.render('index', { results1, results2, totalProducts: results3[0].total_products, productsGroup1, productsGroup2, productsGroup3 });
+        res.render('index', { results1, results2, totalProducts: results3[0].total_products, productsGroup1, productsGroup2, productsGroup3, results5, results6, results7 });
         
     } catch (error) {
         console.error(error);
@@ -92,13 +104,99 @@ function executeQuery(query) {
 
 app.get('/', (req, res) => {
     res.render('index')
-})
+});
+
+//************ page-global routes *********
+app.get('/page-global', async (req, res) => {
+    try {
+        // Primeira Query
+        const query1 = 'SELECT * FROM departments';
+        const results1 = await executeQuery(query1);
+
+        // Segunda Query
+        const query2 = 'SELECT company_name FROM companies ORDER BY RAND() LIMIT 9';
+        const results2 = await executeQuery(query2);
+
+        // Terceira Query
+        const query3 = 'SELECT COUNT(*) AS total_products FROM products';
+        const results3 = await executeQuery(query3);
+
+        // Quarta query
+        const query4 = 'SELECT p.*, c.company_name AS company_name FROM products p JOIN companies c ON p.company_id = c.id ORDER BY RAND() LIMIT 26';
+        const products = await executeQuery(query4);
+
+        // Quinta query
+        const query5 = 'SELECT p.* FROM products p JOIN sub_departments sd ON p.sub_department_id = sd.id JOIN departments d ON sd.department_id = d.id WHERE d.id = 2 ORDER BY RAND() LIMIT 5';
+        const results5 = await executeQuery(query5);
+
+        // Sexta query
+        const query6 = 'SELECT p.* FROM products p JOIN sub_departments sd ON p.sub_department_id = sd.id JOIN departments d ON sd.department_id = d.id WHERE d.id = 3 ORDER BY RAND() LIMIT 5';
+        const results6 = await executeQuery(query6);
+
+        // Setima query
+        const query7 = 'SELECT p.* FROM products p JOIN sub_departments sd ON p.sub_department_id = sd.id JOIN departments d ON sd.department_id = d.id WHERE d.id = 6 ORDER BY RAND() LIMIT 5';
+        const results7 = await executeQuery(query7);
+
+        // Dividir os produtos em dois grupos de 4 produtos cada (Quarta query)
+        const productsGroup1 = products.slice(0, 6);
+        const productsGroup2 = products.slice(6, 12);
+        const productsGroup3 = products.slice(12, 18);
+        const productsGroup4 = products.slice(18, 26);
+
+        // Formatar os preços com duas casas decimais
+        products.forEach(product => {
+            product.formatted_price = product.price.toFixed(2);
+            if (product.is_promotion) {
+                product.formatted_promotion_price = product.promotion_price.toFixed(2);
+            }
+        });
+
+        // Renderizar a página EJS com os resultados
+        res.render('page-global', { results1, results2, totalProducts: results3[0].total_products, productsGroup1, productsGroup2, productsGroup3, productsGroup4, results5, results6, results7 });
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao processar as queries.');
+    }
+
+});
+
+function executeQuery(query) {
+    return new Promise((resolve, reject) => {
+        connection.query(query, (error, results, fields) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
 
 
 //************ page-category routes *********
 app.get('/page-category', (req, res) => {
-    res.render('page-category')
+    try {
+        res.render('page-category')
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao processar as queries.');
+    }
 })
+
+function executeQuery(query) {
+    return new Promise((resolve, reject) => {
+        connection.query(query, (error, results, fields) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
 
 //************ page-offer routes *********
 app.get('/page-offer', (req, res) => {
