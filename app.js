942e2076aa21e5043962a6d9a2ebd9649cb2050d
Tsousa20 <alexandre.gueidao.14@gmail.com>
2024-07-01 19:22:38 +0100

    Update 1/07

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.get('/', async (req, res) => {
         // Dividir os produtos em dois grupos de 4 produtos cada (Quarta query)
         const productsGroup1 = products.slice(0, 6);
         const productsGroup2 = products.slice(6, 12);
-        const productsGroup3 = products.slice(8, 16);
+        const productsGroup3 = products.slice(12, 20);
 
         // Formatar os preços com duas casas decimais
         products.forEach(product => {
@@ -174,109 +174,15 @@ function executeQuery(query) {
     });
 }
 
+app.get('/search', async (req, res) => {
+    const searchTerm = req.query.term;
 
-app.get('/index', async (req, res) => {
-    try {
-        // Primeira Query
-        const query1 = 'SELECT * FROM departments';
-        const results1 = await executeQuery(query1);
+    const query = 'SELECT * FROM products WHERE product_name LIKE ? OR product_reference LIKE ?';
+    const termLike = `%${searchTerm}%`;
+    const results = await executeQuery(query, [termLike, termLike]);
 
-        // Segunda Query
-        const query2 = 'SELECT company_name FROM companies ORDER BY RAND() LIMIT 9';
-        const results2 = await executeQuery(query2);
-
-        // Terceira Query
-        const query3 = 'SELECT COUNT(*) AS total_products FROM products';
-        const results3 = await executeQuery(query3);
-
-        // Quarta query
-        const query4 = 'SELECT p.*, c.company_name AS company_name FROM products p JOIN companies c ON p.company_id = c.id ORDER BY RAND() LIMIT 20';
-        const products = await executeQuery(query4);
-
-        // Quinta query
-        const query5 = 'SELECT p.* FROM products p JOIN sub_departments sd ON p.sub_department_id = sd.id JOIN departments d ON sd.department_id = d.id WHERE d.id = 2 ORDER BY RAND() LIMIT 5';
-        const results5 = await executeQuery(query5);
-
-        // Sexta query
-        const query6 = 'SELECT p.* FROM products p JOIN sub_departments sd ON p.sub_department_id = sd.id JOIN departments d ON sd.department_id = d.id WHERE d.id = 3 ORDER BY RAND() LIMIT 5';
-        const results6 = await executeQuery(query6);
-
-        // Setima query
-        const query7 = 'SELECT p.* FROM products p JOIN sub_departments sd ON p.sub_department_id = sd.id JOIN departments d ON sd.department_id = d.id WHERE d.id = 6 ORDER BY RAND() LIMIT 5';
-        const results7 = await executeQuery(query7);
-
-        // Dividir os produtos em dois grupos de 4 produtos cada (Quarta query)
-        const productsGroup1 = products.slice(0, 6);
-        const productsGroup2 = products.slice(6, 12);
-        const productsGroup3 = products.slice(8, 16);
-
-        // Formatar os preços com duas casas decimais
-        products.forEach(product => {
-            product.formatted_price = product.price.toFixed(2);
-            if (product.is_promotion) {
-                product.formatted_promotion_price = product.promotion_price.toFixed(2);
-            }
-        });
-
-        // Oitava query -> Vai buscar 1 produto random para a promoção
-        const query8 = 'SELECT p.*, c.company_name AS company_name FROM products p JOIN companies c ON p.company_id = c.id WHERE p.is_promotion = 1 ORDER BY RAND() LIMIT 1';
-        const results8 = await executeQuery(query8);
-        const productBig = results8[0];
-
-        // Formatar os preços com duas casas decimais
-        productBig.formatted_price = productBig.price.toFixed(2);
-        productBig.formatted_promotion_price = productBig.promotion_price.toFixed(2);   
-
-        // Nona query -> vai buscar os departamentos e sub-departamentos para o menu lateral
-        const query9 = 'SELECT d.id AS department_id, d.name_depart AS department_name, d.icon_depart AS icon_depart, sd.id AS sub_department_id, sd.name_sub_depart AS sub_department_name FROM departments d LEFT JOIN sub_departments sd ON d.id = sd.department_id';
-        const results9 = await executeQuery(query9);
-
-        // Agrupar os resultados por departamento
-        const departments = results9.reduce((acc, row) => {
-            const { department_id, department_name, icon_depart, sub_department_id, sub_department_name } = row;
-            if (!acc[department_id]) {
-                acc[department_id] = {
-                    id: department_id,
-                    name: department_name,
-                    icon: icon_depart,
-                    subDepartments: []
-                };
-            }
-            if (sub_department_id) {
-                acc[department_id].subDepartments.push({
-                    id: sub_department_id,
-                    name: sub_department_name
-                });
-            }
-            return acc;
-        }, {});
-
-        const departmentList = Object.values(departments);
-
-        // Decima Query -> vai buscar os sub-departamentos para o nav superior
-        const query10 = 'SELECT * FROM sub_departments ORDER BY RAND() LIMIT 9';
-        const results10 = await executeQuery(query10);
-
-        // Cart Query -> Vai buscar os itens do carrinho do cliente
-        if (req.session.user) {
-            const userId = req.session.user.id;
-    
-            const cartQuery = 'SELECT c.product_id, c.quantity, p.product_name, p.price, p.main_img FROM carts c JOIN products p ON c.product_id = p.id WHERE c.company_id = ?';
-            const cartItems = await executeQuery(cartQuery, [userId]);
-    
-            res.render('index', { isAuthenticated:true, results1, results2, totalProducts: results3[0].total_products, productsGroup1, productsGroup2, productsGroup3, results5, results6, results7, productBig, departments: departmentList, results10, cartItems });
-        } else {
-            res.render('index', { isAuthenticated:false, results1, results2, totalProducts: results3[0].total_products, productsGroup1, productsGroup2, productsGroup3, results5, results6, results7, productBig, departments: departmentList, results10 });
-        }
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Erro ao processar as queries.');
-    }
-
+    res.json({ success: true, products: results });
 });
-
-
 
 
 //************ page-category routes *********
@@ -828,6 +734,10 @@ app.get('/page-single/:id', async (req, res) => {
         const query10 = 'SELECT * FROM sub_departments ORDER BY RAND() LIMIT 9';
         const results10 = await executeQuery(query10);
 
+        const query11 = 'SELECT d.id AS department_id FROM products p INNER JOIN sub_departments sd ON p.sub_department_id = sd.id INNER JOIN departments d ON sd.department_id = d.id WHERE p.id = ?';
+        const results11 = await executeQuery(query11, [productId])
+        const departId = results11[0].department_id;
+
         // Cart Query -> Vai buscar os itens do carrinho do cliente
         if (req.session.user) {
             const userId = req.session.user.id;
@@ -835,9 +745,9 @@ app.get('/page-single/:id', async (req, res) => {
             const cartQuery = 'SELECT c.product_id, c.quantity, p.product_name, p.price, p.main_img, p.min_order FROM carts c JOIN products p ON c.product_id = p.id WHERE c.company_id = ?';
             const cartItems = await executeQuery(cartQuery, [userId]);
             
-            res.render('page-single', { isAuthenticated:true, results1, results2, totalProducts: results3[0].total_products, results4, results5, results6, productDetails, results8, departments: departmentList, results10, cartItems });
+            res.render('page-single', { isAuthenticated:true, results1, results2, totalProducts: results3[0].total_products, results4, results5, results6, productDetails, results8, departments: departmentList, results10, departId, cartItems });
         } else {
-            res.render('page-single', { isAuthenticated:false, results1, results2, totalProducts: results3[0].total_products, results4, results5, results6, productDetails, results8, departments: departmentList, results10 });
+            res.render('page-single', { isAuthenticated:false, results1, results2, totalProducts: results3[0].total_products, results4, results5, results6, productDetails, results8, departments: departmentList, results10, departId });
         }
     } catch (error) {
         console.error(error);
@@ -895,9 +805,12 @@ app.get('/page-company/:id', async (req, res) => {
         const queryDetails = 'SELECT * FROM companies WHERE id = ?';
         const resultsDetails = await executeQuery(queryDetails, [companyId]);
 
-        const query = 'SELECT p.*, d.name_depart FROM products p INNER JOIN sub_departments sd ON p.sub_department_id = sd.id INNER JOIN departments d ON sd.department_id = d.id WHERE p.company_id = ?';
+        const query = 'SELECT DISTINCT d.name_depart FROM products p INNER JOIN sub_departments sd ON p.sub_department_id = sd.id INNER JOIN departments d ON sd.department_id = d.id WHERE p.company_id = ?';
         const products = await executeQuery(query, [companyId]);
     
+        const queryDepartId = 'SELECT d.id AS department_id FROM products p INNER JOIN sub_departments sd ON p.sub_department_id = sd.id INNER JOIN departments d ON sd.department_id = d.id WHERE p.company_id = ? ORDER BY p.id LIMIT 1';
+        const resultsDepartId = await executeQuery(queryDepartId, [companyId]);
+        const departId = resultsDepartId[0].department_id;
         
 
         const query8 = 'SELECT p.*, c.company_name FROM products p JOIN companies c ON p.company_id = c.id WHERE p.company_id = ?';
@@ -948,9 +861,9 @@ app.get('/page-company/:id', async (req, res) => {
             const cartQuery = 'SELECT c.product_id, c.quantity, p.product_name, p.price, p.main_img, p.min_order FROM carts c JOIN products p ON c.product_id = p.id WHERE c.company_id = ?';
             const cartItems = await executeQuery(cartQuery, [userId]);
             
-            res.render('page-company', { isAuthenticated:true, results1, results2, totalProducts: results3[0].total_products, results4, results5, results6, companyName, results8, departments: departmentList, results10, companyDetails: resultsDetails[0], products, cartItems });
+            res.render('page-company', { isAuthenticated:true, results1, results2, totalProducts: results3[0].total_products, results4, results5, results6, companyName, results8, departments: departmentList, results10, companyDetails: resultsDetails[0], products, departId, cartItems });
         } else {
-            res.render('page-company', { isAuthenticated:false, results1, results2, totalProducts: results3[0].total_products, results4, results5, results6, companyName, results8, departments: departmentList, results10, companyDetails: resultsDetails[0], products });
+            res.render('page-company', { isAuthenticated:false, results1, results2, totalProducts: results3[0].total_products, results4, results5, results6, companyName, results8, departments: departmentList, results10, companyDetails: resultsDetails[0], products, departId });
         }
     } catch (error) {
         console.error(error);
@@ -1455,6 +1368,15 @@ app.post('/login', async (req, res) => {
     }
 });
 
+app.post('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Erro ao destruir a sessão:', err);
+            return res.status(500).json({ success: false, message: 'Error logging out. Please try again later.' });
+        }
+        res.json({ success: true, message: 'Logout successful!' });
+    });
+});
 
 
 
--- views/script-2.js
+++ views/script-2.js
@@ -36,3 +36,76 @@ function openFAQQuestion(id) {
         element.scrollIntoView({ behavior: 'smooth', block: 'start' }); // rolar até a questão
     }
 }
+
+document.getElementById('searchInput').addEventListener('input', function(event) {
+    const searchTerm = event.target.value.trim(); // Obter o termo de busca
+
+    // Verificar se o termo de busca é vazio
+    if (searchTerm === '') {
+        hideSearchResults(); // Ocultar resultados se o campo estiver vazio
+        return;
+    }
+
+    // Fazer requisição AJAX para buscar produtos
+    fetch(`/search?term=${encodeURIComponent(searchTerm)}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                displaySearchResults(data.products); // Exibir os resultados da busca
+            } else {
+                console.error('Erro ao buscar produtos:', data.message);
+                alert('Erro ao buscar produtos. Por favor, tente novamente mais tarde.');
+            }
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+            alert('Erro na requisição. Por favor, tente novamente mais tarde.');
+        });
+});
+
+// Função para exibir os resultados da busca
+function displaySearchResults(products) {
+    const searchResultsContainer = document.getElementById('searchResults');
+    searchResultsContainer.innerHTML = ''; // Limpar resultados anteriores
+
+    products.forEach(product => {
+        const resultItem = document.createElement('div');
+        resultItem.classList.add('search-result-item');
+
+        const img = document.createElement('img');
+        img.src = `/assets/empresas_img/${product.main_img}`; // Definir o src da imagem
+        img.classList.add('product-image');
+
+        
+        const text = document.createElement('p');
+        text.textContent = `${product.product_name} - ${product.product_reference}`;
+        text.classList.add('product-details');
+
+        resultItem.appendChild(img);
+        resultItem.appendChild(text);
+        
+        
+        // Adicionar evento de clique para selecionar o produto
+        resultItem.addEventListener('click', function() {
+            navigateToProductDetail(product.id); // Redirecionar para a página de detalhes do produto
+        });
+
+        searchResultsContainer.appendChild(resultItem);
+    });
+
+    showSearchResults(); // Mostrar resultados de busca
+};
+
+// Função para mostrar os resultados de busca
+function showSearchResults() {
+    document.getElementById('searchResults').style.display = 'block';
+};
+
+// Função para ocultar os resultados de busca
+function hideSearchResults() {
+    document.getElementById('searchResults').style.display = 'none';
+};
+
+function navigateToProductDetail(productId) {
+    window.location.href = `/page-single/${productId}`;
+};
